4b341b97c50300dad3dc03c64cbb495be6d0f7d7
Mickael Lamasuta <mickael.lamasuta@etu.unice.fr>
2023-04-06 09:13:19 +0200

    vuetify setup

--- DemoKGViz-vue/src/main.js
+++ DemoKGViz-vue/src/main.js
@@ -2,12 +2,30 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import FloatingVue from 'floating-vue'
 import {store} from "@/store"
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import {aliases, mdi} from 'vuetify/iconsets/mdi'
 
 /* floating-vue configuration */
 // Display the tooltip quickly (50ms instead of 200ms)
 FloatingVue.options.themes.tooltip.delay.show = 50;
 
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        }
+    }
+})
+
 const app = createApp(App)
 app.use(store)
 app.use(FloatingVue)
+app.use(vuetify)
 app.mount('#app')
