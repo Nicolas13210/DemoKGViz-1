f29f483b5dded808002948e09858191e92c3a36a
dsv-thomas <thomas.de-sousa-vieira@etu.unice.fr>
2023-04-22 23:38:33 +0200

    Add comment

--- DemoKGViz-vue/src/utils/dataTransformation.js
+++ DemoKGViz-vue/src/utils/dataTransformation.js
@@ -1,3 +1,20 @@
+/**
+ * Allows you to clean the data to a format :
+ * {
+ *   "values": [{
+ *       "stationName": "NICE",
+ *       "date": "2021-12-01",
+ *       "temp_avg": 8.8,
+ *       "temp_min": 4.4,
+ *       "temp_max": 13.2,
+ *       "temp_diff": 8.8,
+ *       "rainfall": "0"
+ *     }]
+ * }
+ *
+ * @param inputData
+ * @returns {{values: []}}
+ */
 export function transformData(inputData) {
     const outputData = {
         "values": []
