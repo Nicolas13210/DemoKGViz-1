57b0391d92e92d42eda6b719f0959c3af6f3ad2a
dsv-thomas <thomas.de-sousa-vieira@etu.unice.fr>
2023-05-10 22:07:21 +0200

    Fixed dynamic graphic display

--- DemoKGViz-vue/src/components/tabs/ChartResult.vue
+++ DemoKGViz-vue/src/components/tabs/ChartResult.vue
@@ -1,32 +1,121 @@
 <template>
-    <div id="visualisation">
-        <div class="groupVisualisation">
-            <LineChart></LineChart>
-        </div>
+  <div id="visualisation">
+    <div class="groupVisualisation">
+      <LineChart v-if="getAllData !== undefined" :chartData="getSelectAndConcatAttributes" :chartAttributes="this.properties"></LineChart>
     </div>
+  </div>
 </template>
 
 <script>
 import LineChart from "@/components/tabs/charts/LineChart.vue";
+import {buildQuery_tmpRainStation} from "@/queries/queries";
+import {parametersAvailable} from "@/utils/parametersAvailable";
 
 export default {
-    name: "ChartResult",
-    components: {
-        LineChart,
+  name: "ChartResult",
+  components: {
+    LineChart,
+  },
+  data() {
+    return {
+      // Property for the chart.
+      properties: []
     }
+  },
+  created() {
+   // this.fetchData();
+  },
+  computed: {
+    getSelectedStation() {
+      return (this.$store.getters.getSelectedStations).map(station => station.stationName.value);
+    },
+    getStartDate() {
+      return this.convertDateToYearMonthDay(this.$store.getters.getStartDate);
+    },
+    getEndDate() {
+      return this.convertDateToYearMonthDay(this.$store.getters.getEndDate);
+    },
+    getSelectAndConcatAttributes() {
+      return this.selectAndConcatAttributes(this.$store.getters.getAllData.result);
+    },
+    getAllData() {
+      return this.$store.getters.getAllData.result;
+    },
+  },
+  methods: {
+    isParameterAvailable(parameters) {
+      let properties = [];
+      for (let parameter in parameters) {
+        let item = parametersAvailable(parameters[parameter], "LINE")
+        if (item !== undefined) {
+          properties.push({
+            title: item.param,
+            jsonPath: item.jsonPath,
+            color: "#ffa600"
+          })
+        }
+      }
+      return properties;
+    },
+    convertDateToYearMonthDay(date) {
+      const year = date.getFullYear();
+      const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+      const day = String(date.getDate()).padStart(2, '0');
+      return `${year}-${month}-${day}`;
+    },
+    selectAndConcatAttributes(json) {
+      let properties = this.isParameterAvailable(this.$store.getters.getParameters)
+      this.properties = properties;
+      const attributes = ["date"].concat(properties.map(element => element.jsonPath));
+      let result = [];
+      for (let attribute of attributes) {
+        let values = [];
+        for (let valueObj of json.values) {
+          let value = valueObj[attribute];
+          values.push(value);
+        }
+        result.push({"attribute": attribute, "values": values});
+      }
+      return result;
+    },
+    //TODO: to move ???
+    fetchData() {
+      // FIXME: The following "IF" is temporarily used to not stress the back-end server during our tests. So the for-loop will always loop on one station.
+      if (this.getSelectedStation.length === 1) {
+        // Draw charts according to the removed/added stations.
+        for (const selectedStation of this.getSelectedStation) {
+          this.$store.dispatch("fetchWeatherData", buildQuery_tmpRainStation(selectedStation, this.getStartDate, this.getEndDate));
+        }
+      }
+    },
+  },
+  watch: {
+    getSelectedStation(newCount, oldCount) {
+      this.fetchData();
+      console.log("call update getSelectedStation")
+    },
+    getStartDate(newCount, oldCount) {
+      this.fetchData();
+      console.log("call update getStartDate")
+    },
+    getEndDate(newCount, oldCount) {
+      this.fetchData();
+      console.log("call update getEndDate")
+    }
+  }
 }
 </script>
 
 <style scoped>
 div#visualisation {
-    margin-top: 20px;
+  margin-top: 20px;
 }
 
 div.groupVisualisation {
-    /* Centrer les boutons au center de la page */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 25px;
+  /* Centrer les boutons au center de la page */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 25px;
 }
 </style>
--- DemoKGViz-vue/src/components/tabs/charts/LineChart.vue
+++ DemoKGViz-vue/src/components/tabs/charts/LineChart.vue
@@ -1,122 +1,43 @@
 <template>
-  <Line v-if="chartData !== undefined" :data="chartData" :options="chartOptions"/>
+    <Line :data="processData" :options="chartOptions"/>
 </template>
 
 <script>
 import {Line} from 'vue-chartjs'
 import {CategoryScale, Chart as ChartJS, Legend, LinearScale, LineElement, PointElement, Title, Tooltip} from 'chart.js'
-import {buildQuery_tmpRainStation} from "@/queries/queries";
-import {parametersAvailable} from "@/utils/parametersAvailable";
 
 ChartJS.register(CategoryScale, LinearScale, LineElement, Title, Tooltip, Legend, PointElement)
 
 export default {
   name: "LineChart",
+  props: {
+    chartData: Object,
+    chartAttributes: Object,
+  },
   components: {
     Line
   },
-  data() {
-    return {
-      // Property for the chart.
-      properties: [
-        /*
-                {
-                  title: "Tavg",
-                  jsonPath: "temp_avg",
-                  color: "#ffa600"
-
-                },
-                {
-                  title: "Tmin",
-                  jsonPath: "temp_min",
-                  color: "#3c83ff"
-                }
-        */
-      ]
-    }
-  },
-  created() {
-    this.drawCharts();
-  },
-  methods: {
-    //TODO: move some methods to Chart result
-    convertDateToYearMonthDay(date) {
-      const year = date.getFullYear();
-      const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-      const day = String(date.getDate()).padStart(2, '0');
-      return `${year}-${month}-${day}`;
-    },
-    selectAndConcatAttributes(json) {
-      const attributes = ["date"].concat(this.properties.map(element => element.jsonPath));
-      let result = [];
-      for (let attribute of attributes) {
-        let values = [];
-        for (let valueObj of json.values) {
-          let value = valueObj[attribute];
-          values.push(value);
-        }
-        result.push({"attribute": attribute, "values": values});
-      }
-      return result;
-    },
-    drawCharts() {
-      const selectedStations = (this.$store.getters.getSelectedStations).map(station => station.stationName.value);
-
-      // FIXME: The following "IF" is temporarily used to not stress the back-end server during our tests. So the for-loop will always loop on one station.
-
-      if (selectedStations.length === 1) {
-        // Draw charts according to the removed/added stations.
-        const startDate = this.convertDateToYearMonthDay(this.$store.getters.getStartDate);
-        const endDate = this.convertDateToYearMonthDay(this.$store.getters.getEndDate);
-
-        for (const selectedStation of selectedStations) {
-          this.$store.dispatch("fetchWeatherData", buildQuery_tmpRainStation(selectedStation, startDate, endDate));
-        }
-      }
-    },
-    isParameterAvailable(parameters) {
-      this.properties = [];
-      for (let parameter in parameters) {
-        let item = parametersAvailable(parameters[parameter], "LINE")
-        if (item !== undefined) {
-          this.properties.push({
-            title: item.param,
-            jsonPath: item.jsonPath,
-            //TODO: change color
-            color: "#ffa600"
-          })
-        }
-      }
-    },
-  },
   computed: {
-    //TODO: REFACTOR THIS METHOD
-    chartData() {
-      this.isParameterAvailable(this.$store.getters.getParameters)
-
+    processData() {
       if (this.$store.getters.getAllData.length === 0) {
         // No data loaded.
         return undefined;
       }
-
-      let concatAttributes = this.selectAndConcatAttributes(this.$store.getters.getAllData.result)
-
-      const labels = concatAttributes.find(item => item.attribute === "date").values;
+      const labels = this.chartData.find(item => item.attribute === "date").values;
       const datasets = [];
-      for (let property of this.properties) {
+      for (let property of this.chartAttributes) {
         datasets.push({
           label: property.title,
           backgroundColor: property.color,
-          data: concatAttributes.find(item => item.attribute === property.jsonPath).values
+          data: this.chartData.find(item => item.attribute === property.jsonPath).values
         });
       }
-
-      const data = {
+      const data2 = {
         labels: labels,
         datasets: datasets
       }
-      console.log("lineChart computed:", data);
-      return data;
+      console.log("lineChart computed:", data2);
+      return data2;
 
     },
     chartOptions() {
