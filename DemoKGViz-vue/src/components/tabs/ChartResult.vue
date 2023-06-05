627be765c59c53775b11c609a21ffeb071caa43c
dsv-thomas <thomas.de-sousa-vieira@etu.unice.fr>
2023-06-05 09:12:00 +0200

    Code formatting

--- DemoKGViz-vue/src/components/tabs/ChartResult.vue
+++ DemoKGViz-vue/src/components/tabs/ChartResult.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="visualisation">
-    <div class="groupVisualisation">
-      <ComboChart v-if="getWeather !== undefined" :chartData="getWeather"></ComboChart>
-      <PolarChart v-if="getWeatherNbDay !== undefined" :chartData="getWeatherNbDay"></PolarChart>
+    <div id="visualisation">
+        <div class="groupVisualisation">
+            <ComboChart v-if="getWeather !== undefined" :chartData="getWeather"></ComboChart>
+            <PolarChart v-if="getWeatherNbDay !== undefined" :chartData="getWeatherNbDay"></PolarChart>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -12,33 +12,33 @@ import ComboChart from "@/components/tabs/charts/ComboChart.vue";
 import PolarChart from "@/components/tabs/charts/PolarChart.vue";
 
 export default {
-  name: "ChartResult",
-  components: {
-    ComboChart,
-    PolarChart
-  },
-  computed: {
-    getWeather() {
-      return this.$store.getters.getWeather?.result;
+    name: "ChartResult",
+    components: {
+        ComboChart,
+        PolarChart
     },
-    getWeatherNbDay() {
-      return this.$store.getters.getWeatherNbDay?.result;
-    },
-  }
+    computed: {
+        getWeather() {
+            return this.$store.getters.getWeather?.result;
+        },
+        getWeatherNbDay() {
+            return this.$store.getters.getWeatherNbDay?.result;
+        },
+    }
 }
 </script>
 
 <style scoped>
 div#visualisation {
-  margin-top: 20px;
+    margin-top: 20px;
 }
 
 div.groupVisualisation {
-  /* Centrer les boutons au center de la page */
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 25px;
+    /* Centrer les boutons au center de la page */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 25px;
 }
 </style>
--- DemoKGViz-vue/src/components/tabs/charts/ComboChart.vue
+++ DemoKGViz-vue/src/components/tabs/charts/ComboChart.vue
@@ -73,31 +73,34 @@ export default {
             }
             return result;
         },
-      selectAndConcatAttributesDateComparaison(json) {
-        let properties = this.setProperties(this.$store.getters.getParameters)
-        this.properties = properties;
-        const attributes =  ["date"].concat(properties.map(element => element.jsonPath));
+        selectAndConcatAttributesDateComparaison(json) {
+            let properties = this.setProperties(this.$store.getters.getParameters)
+            this.properties = properties;
+            const attributes = ["date"].concat(properties.map(element => element.jsonPath));
 
-        let result = [];
-        for (let attribute of attributes) {
-          let values = [];
-          for (let valueObj of json.values) {
-            let value = valueObj[attribute];
-            let date = valueObj["date"];
-            values.push({"station": valueObj['stationName'], "value": value, "year": date.substring(0, 4)});
-          }
-          values.forEach(item => {
-            const existingItem = result.find(outputItem => outputItem.station === item.station);
+            let result = [];
+            for (let attribute of attributes) {
+                let values = [];
+                for (let valueObj of json.values) {
+                    let value = valueObj[attribute];
+                    let date = valueObj["date"];
+                    values.push({"station": valueObj['stationName'], "value": value, "year": date.substring(0, 4)});
+                }
+                values.forEach(item => {
+                    const existingItem = result.find(outputItem => outputItem.station === item.station);
 
-            if (existingItem) {
-              existingItem.data.push({"attribute": attribute, "value": item.value, "year": item.year});
-            } else {
-              result.push({station: item.station, data: [{"attribute": attribute, "value": item.value, "year": item.year}]});
+                    if (existingItem) {
+                        existingItem.data.push({"attribute": attribute, "value": item.value, "year": item.year});
+                    } else {
+                        result.push({
+                            station: item.station,
+                            data: [{"attribute": attribute, "value": item.value, "year": item.year}]
+                        });
+                    }
+                });
             }
-          });
-        }
-        return result;
-      },
+            return result;
+        },
 
     },
     computed: {
@@ -106,39 +109,39 @@ export default {
                 // No data loaded.
                 return undefined;
             }
-          let computedData, labels;
-          if(this.$store.getters.getComparison) {
-            computedData = this.selectAndConcatAttributesDateComparaison(this.chartData);
-             labels = [...new Set(computedData[0].data.filter(item => item.attribute === "date").map(item => item.value.slice(5)))];
-          } else {
-            computedData = this.selectAndConcatAttributes(this.chartData);
-             labels = [...new Set(computedData[0].data.filter(item => item.attribute === "date").map(item => item.value))];
-          }
-
-          const years = [...new Set(computedData[0].data.map(entry => entry.year))];
-
-          const datasets = [];
-          for(let year of years) {
-            for (let stationData of computedData) {
-              // For each station (station: XXX, data: [{attribute: XXX, value: XXX}]).
-                for (let property of this.properties) {
-                  const titleLabel = property.title + "(" + stationData.station + ")" + ((this.$store.getters.getComparison) ? " - " + year : "");
-                  let data;
-                  if(this.$store.getters.getComparison) {
-                     data = stationData.data.filter(item => item.attribute === property.jsonPath && item.year === year).map(item => item.value)
-                  } else {
-                     data = stationData.data.filter(item => item.attribute === property.jsonPath).map(item => item.value)
-                  }
-                  datasets.push({
-                    label: titleLabel,
-                    backgroundColor: randomColor({seed: titleLabel}),
-                    data:data ,
-                    type: property.type,
-                    displayUnit: property.displayUnit
-                  });
-              }
+            let computedData, labels;
+            if (this.$store.getters.getComparison) {
+                computedData = this.selectAndConcatAttributesDateComparaison(this.chartData);
+                labels = [...new Set(computedData[0].data.filter(item => item.attribute === "date").map(item => item.value.slice(5)))];
+            } else {
+                computedData = this.selectAndConcatAttributes(this.chartData);
+                labels = [...new Set(computedData[0].data.filter(item => item.attribute === "date").map(item => item.value))];
             }
-          }
+
+            const years = [...new Set(computedData[0].data.map(entry => entry.year))];
+
+            const datasets = [];
+            for (let year of years) {
+                for (let stationData of computedData) {
+                    // For each station (station: XXX, data: [{attribute: XXX, value: XXX}]).
+                    for (let property of this.properties) {
+                        const titleLabel = property.title + "(" + stationData.station + ")" + ((this.$store.getters.getComparison) ? " - " + year : "");
+                        let data;
+                        if (this.$store.getters.getComparison) {
+                            data = stationData.data.filter(item => item.attribute === property.jsonPath && item.year === year).map(item => item.value)
+                        } else {
+                            data = stationData.data.filter(item => item.attribute === property.jsonPath).map(item => item.value)
+                        }
+                        datasets.push({
+                            label: titleLabel,
+                            backgroundColor: randomColor({seed: titleLabel}),
+                            data: data,
+                            type: property.type,
+                            displayUnit: property.displayUnit
+                        });
+                    }
+                }
+            }
             const data = {
                 labels: labels,
                 datasets: datasets
